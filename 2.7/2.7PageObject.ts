3f20763c29e6cebe90d38f268deab482df0398ac
unknown <nbackues333@gmail.com>
2024-09-05 21:44:32 -0500

    Update Homework

--- 2.7/2.7PageObject.ts
+++ 2.7/2.7PageObject.ts
@@ -4,7 +4,7 @@ import {By} from 'selenium-webdriver';
 export class pageObject extends BasePage {
     addEm: By = By.name('addEmployee');
     newEm: By = By.xpath('//li[text()="New Employee"]');
-    namInp: By = By.xpath('(//input[@class="materialInput"][1])');
+    namInp: By = By.xpath('(//input[@class="materialInput"])[1]');
     phoInp: By = By.name('phoneEntry');
     titleInp: By = By.name('titleEntry');
     saveBtn: By = By.id('saveBtn');
